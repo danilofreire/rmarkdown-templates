042a36865938598f92e7a6245582700443ad9805
Danilo Freire <danilofreire@users.noreply.github.com>
2018-06-10 01:36:57 -0300

    typo

--- run.R
+++ run.R
@@ -1,4 +1,4 @@
-# If you're not using RStudio to knit the RMd file, just enter the commands above to create the pdf:
+# If you're not using RStudio to knit the RMd file, just enter the commands below to create the pdf:
 # install.packages("rmarkdown") 
 library(rmarkdown)
 rmarkdown::render('article.Rmd', output_file='article.pdf')
