a4abd847dbcb4bf66bdb4651e9a959b37f468739
Danilo Freire <danilofreire@gmail.com>
2019-07-26 02:41:34 -0400

    add title page and letter templates

--- run.R
+++
@@ -1,4 +0,0 @@
-# If you're not using RStudio to knit the RMd file, just enter the commands below to create the pdf:
-# install.packages("rmarkdown") 
-library(rmarkdown)
-rmarkdown::render('article.Rmd', output_file='article.pdf')
